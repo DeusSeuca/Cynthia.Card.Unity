1dbb63be39c085afe791d4f22d303b1bba096e65
Cynthia <172587349@qq.com>
2018-08-04 14:46:24 +0800

    创建库完毕

---
+++ src/Cynthia.Card.Unity.Client/Assets/Script/GameMain/GaneEntrance.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class GaneEntrance : MonoBehaviour
+{
+    public GameObject GlobalUI;
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+        DontDestroyOnLoad(GlobalUI);
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ src/Cynthia.Card.Unity.Client/Assets/Script/GlobalUI/MessageBox.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Alsein.Utilities.IO;
+
+public class MessageBox : MonoBehaviour
+{
+    public Text TitleText;
+    public Text MessageText;
+    public Text YesText;
+    public Text NoText;
+    private IAsyncDataEndPoint stream = new AsyncDataEndPoint();
+
+    public Task<bool> Show(string title,string message,string yes = "确定",string no = "取消")
+    {
+        gameObject.SetActive(true);
+        TitleText.text = title;
+        MessageText.text = message;
+        YesText.text = yes;
+        NoText.text = no;
+        return stream.ReceiveAsync<bool>();
+    }
+    public void YesClick()
+    {
+        gameObject.SetActive(false);
+        stream.SendAsync<bool>(true);
+    }
+    public void NoClick()
+    {
+        gameObject.SetActive(false);
+        stream.SendAsync<bool>(false);
+    }
+}
